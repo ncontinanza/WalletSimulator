d7ec7b9917e02e81a0b4b8ee5c1b1a713b030fa1
ncontinanza94@gmail.com <nicolas.continanza@almundo.com>
2020-04-17 10:06:39 -0300

    First commit

---
+++ src/main/java/com/softvision/WalletSimulator/WalletSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.softvision.WalletSimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletSimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/softvision/WalletSimulator/WalletSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softvision.WalletSimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
